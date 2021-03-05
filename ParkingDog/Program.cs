3d2260778313483f3226e1abf7aef121d52e01a1
Dragomir Dragnev <d_dragnev@protonmail.com>
2021-03-05 12:15:44 +0200

    Add basic command processing

--- ParkingDog/Program.cs
+++ ParkingDog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ParkingDog
 {
@@ -6,7 +7,56 @@ namespace ParkingDog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                var input = ReadInputString();
+                ProcessCommand(input);
+            }
+        }
+
+        private static string ReadInputString()
+        {
+            return Console.ReadLine();
+        }
+
+        private static void ProcessCommand(string inputString)
+        {
+            var input = inputString.Split();
+            var command = input[0];
+            var arguments = input.Skip(1).ToArray();
+            
+            switch (command)
+            {
+                case "паркинг":
+                    var name = input[1];
+                    var automobiles = ParseNumericInput(input[2]);
+                    var lightCommercialVehicles = ParseNumericInput(input[3]);
+                    var heavyGoodsVehicles = ParseNumericInput(input[4]);
+                    
+                    break;
+                case "край":
+                    TerminateApplication();
+                    break;
+                default:
+                    InformTheUser("Непозната команда.");
+                    break;
+            }
+        }
+
+        private static int ParseNumericInput(string input)
+        {
+            // try.parse
+            return int.Parse(input);
+        }
+
+        private static void TerminateApplication()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void InformTheUser(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
