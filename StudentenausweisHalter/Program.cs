4c9486a8a3cdf394369e9c2c4620136a059afb8f
Dragomir Dragnev <d_dragnev@protonmail.com>
2021-03-05 08:33:47 +0200

    Add project files.

---
+++ StudentenausweisHalter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentenausweisHalter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
